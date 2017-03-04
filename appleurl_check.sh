9718399ece2eb66819155f9ccffb48ac80620167
durbinite <durbinite@users.noreply.github.com>
2017-03-03 18:24:24 -0800

    Delete appleurl_check.sh

--- appleurl_check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# // URL Check with Curl
-while read line ; do
-     statuscode=$(curl -siL $line | awk '/^HTTP/{print $2}')
-     echo -e "${line}\t${statuscode}"  >> URL_Check.txt
- done < list.txt
